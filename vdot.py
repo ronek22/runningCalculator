694d37e0011b0fe1949fbf4be1113cef66e991c1
Jakub Ronkiewicz <jronek3010@gmail.com>
2017-09-07 02:23:57 +0200

    Fix issue with vdot calculation & format output

--- vdot.py
+++ vdot.py
@@ -1,33 +1,34 @@
-import numpy as np
 import pandas as pd
-
-vdot = pd.read_csv('daniels_table_races.csv', delimiter=',',index_col = 'Params')
+vdot = pd.read_csv('daniels_table_races.csv',
+                   delimiter=',', index_col='Params')
 pacesTab = pd.read_csv('paces.csv', delimiter=',', index_col='Params')
 
 
-def compareTimMin(time,timeList,distance):
-	test = len(time.split(':'))
+def compareTimMin(time, timeList, distance):
+    test = len(time.split(':'))
 
-	if test == 2 and distance <= 7:
-		list2=[float('.'.join(i.split(':'))) for i in timeList]
-		time2=float('.'.join(time.split(':')))
-		out = str(min(list2,key=lambda x:abs(x-time2)))
-		out = ':'.join(out.split('.'))
-		ind = vdot.iloc[:,distance-1][vdot.iloc[:,distance-1] == out].index.tolist()[0]
-		return out,ind
-	elif test == 2 and distance > 7:
-		print "Error: time must be in format(hh:mm:ss)"
-		return "",""
-	elif test == 3 and distance > 7:
-		list2 = [int(''.join(i.split(':'))) for i in timeList]
-		time2 = int(''.join(time.split(':')))
-		out = str(min(list2, key=lambda x:abs(x-time2)))
-		out=out[:1]+':'+out[1:3]+':'+out[3:]
-		ind = vdot.iloc[:,distance-1][vdot.iloc[:,distance-1] == out].index.tolist()[0]
-		return out, ind
-	elif test == 3 and distance <= 7:
-		print "Error: time must be in format(mm:ss)"
-		return "",""
+    if test == 2 and distance <= 7:
+        list2 = [float('.'.join(i.split(':'))) for i in timeList]
+        time2 = float('.'.join(time.split(':')))
+        out = str(min(list2, key=lambda x: abs(x - time2)))
+        out = ':'.join(out.split('.'))
+        if len(out) == 4:
+            out += '0'
+        ind = vdot.iloc[:, distance - 1][vdot.iloc[:,distance - 1] == out].index.tolist()[0]
+        return out, ind
+    elif test == 2 and distance > 7:
+        print "Error: time must be in format(hh:mm:ss)"
+        return "", ""
+    elif test == 3 and distance > 7:
+        list2 = [int(''.join(i.split(':'))) for i in timeList]
+        time2 = int(''.join(time.split(':')))
+        out = str(min(list2, key=lambda x: abs(x - time2)))
+        out = out[:1] + ':' + out[1:3] + ':' + out[3:]
+        ind = vdot.iloc[:, distance - 1][vdot.iloc[:,distance - 1] == out].index.tolist()[0]
+        return out, ind
+    elif test == 3 and distance <= 7:
+        print "Error: time must be in format(mm:ss)"
+        return "", ""
 
 
 def vdotCalc():
@@ -48,21 +49,25 @@ def vdotCalc():
     """
     distance = int(raw_input("\n>> "))
     distDic = {
-    	1: '1500m',
-    	2: '1600m',
-    	3: '3km',
-    	4: '3200m',
-    	5: '5km',
-    	6: '10km',
-    	7: '15km',
-    	8: 'HM',
-    	9: 'M'
+        1: '1500m',
+        2: '1600m',
+        3: '3km',
+        4: '3200m',
+        5: '5km',
+        6: '10km',
+        7: '15km',
+        8: 'HM',
+        9: 'M'
     }
 
     time = raw_input("Provide your time: ")
 
-    timeTab,ind = compareTimMin(time,vdot[distDic[distance]], distance)
-    print "NEAREST TIME: ", timeTab
-    print "YOUR VDOT IS: ", ind
-    print "PACES:\n"
-    print pacesTab.ix[ind]
+    timeTab, ind = compareTimMin(time, vdot[distDic[distance]], distance)
+    # print "NEAREST TIME: ", timeTab
+    print "YOUR VDOT IS: ", ind,"\n"
+    print '{:~^20}'.format('PACES')
+    print pacesTab.ix[ind].to_frame()
+    print ""
+    print '{:~^20}'.format('RACING TIMES')
+    print vdot.ix[ind].to_frame().T
+    print ""
