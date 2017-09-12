4f68b2a5cac938768c006a067f60e735b5cf2dae
Jakub Ronkiewicz <jronek3010@gmail.com>
2017-09-12 05:02:54 +0200

    Clean up code & make PEP8 compliant

--- calc.py
+++ calc.py
@@ -1,125 +1,129 @@
-from sys import exit
-from vdot import vdotCalc
+'''Running calculator, that calculates (pace, time,long run, vdot, paces for vdot)'''
 import os
 
-def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+MENU_TEXT = """\nRunning Calculator
+{0}
+1. Long run distance calculation.
+2. Distance & Time -> Pace.
+3. Pace & Distance -> Time.
+4. VDOT calculations & Provide paces.
+5. Exit
+{0}""".format( "=" * 50)
 
-#return pace in minutes
-def stringTempo(pace):
-	tabPace = pace.split(':')
-	minut = int(tabPace[0])
-	sec = float(tabPace[1])/60
-	tempo = minut + sec
-	return tempo
-
-def print0(x):
-	if x <= 9:
-		return "0%d" % x
-	else:
-		return "%d" % x
-
-# return time from string to minutes(float)
-def stringTime(time):
-	tabTime = time.split(':')
-	hour = int(tabTime[0])*60
-	minut = int(tabTime[1])
-	sec = float(tabTime[2])/60
-	time = hour + minut + sec
-	return time
-
-def minutes2Time(minutes):
-	hours = int(minutes/60)
-	minut = int(minutes%60) # reszta z dzielenia przez godzine to minuty
-	seconds = (minutes - int(minutes))*60
-	print "%s:%s:%s" % (print0(hours),print0(minut),print0(seconds))
-
-def pauseEnd():
-	raw_input("Press any key to back to menu.")
-
-def menu():
-	while True:
-		cls()
-		print "\nRunning Calculator"
-		print "=" * 50
-		print "1. Long run distance calculation."
-		print "2. Distance & Time -> Pace."
-		print "3. Pace & Distance -> Time"
-		print "4. VDOT Calculations & Show Paces"
-		print "5. Exit"
-		print "-" * 50
-		print "New functions will be added soon.\n"
-
-		odp = int(raw_input("What do you want to calculate?\n>> "))
-
-		if odp == 1:
-			cls()
-			print """
+LONGRUN_TEXT = """
 Long run is run in comfortable pace,
 It maintain your endurance,
 According to Jack Daniels program,
 run shouldn't last longer than 120 minutes
 or shouldn't have more than 25% of weekly training volume.
-			"""
-			dystans = float(raw_input("How many kilometres do you run in this week?\n>> "))
-			daneTempa = raw_input("What is your long run pace?\n>> ")
-			tempo = stringTempo(daneTempa)
+"""
 
-			s = (dystans/4)/(0.75)
-			czas = s*tempo
+def cls():
+    '''Clear screen depending of os'''
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-			if czas > 120:
-				print "Run in comfortable way for 2 hours."
-				pauseEnd()
-			else:
-				print "Your long run in this week should be: %dkm" % s
-				print "Probably your run will takes: %d minutes" % czas
-				pauseEnd()
 
-		elif odp == 2:
-			cls()
-			print "Distance & Time -> Pace"
-			print "=" * 50
+def string_tempo(pace):
+    '''return pace in minutes'''
+    tab_pace = pace.split(':')
+    minut = int(tab_pace[0])
+    sec = float(tab_pace[1]) / 60
+    tempo = minut + sec
+    return tempo
 
-			dystans = float(raw_input("Distance[km]: "))
-			daneCzas = raw_input("Time[hh:mm:ss]: ")
-			czas = stringTime(daneCzas)
 
-			pace = czas/dystans
-			minutes = int(pace)
-			sec = int((pace - minutes)*60)
+def print0(number):
+    '''add leading 0 to number string, if number is less than 10'''
+    if number <= 9:
+        return "0%d" % number
+    else:
+        return "%d" % number
 
-			print ""
-			if sec > 9:
-				print "If you ran %.2f km in %s, your pace will be %d:%d min/km" % (dystans,daneCzas,minutes,sec)
-			else:
-				print "If you ran %.2f km in %s, your pace will be %d:0%d min/km" % (dystans,daneCzas,minutes,sec)
-			pauseEnd()
+def string_time(time):
+    '''return time from string to minutes(float)'''
+    tab_time = time.split(':')
+    hour = int(tab_time[0]) * 60
+    minut = int(tab_time[1])
+    sec = float(tab_time[2]) / 60
+    time = hour + minut + sec
+    return time
 
-		elif odp == 3:
-			cls()
-			print "Pace & Distance -> Time"
-			print "=" * 50
 
-			daneTempa = raw_input("Pace[min/km]: ")
-			tempo = stringTempo(daneTempa)
-			dystans = float(raw_input("Distance[km]: "))
+def minutes2time(minutes):
+    '''convert from minutes to hh:mm:ss and print it out'''
+    hours = int(minutes / 60)
+    minut = int(minutes % 60)  # reszta z dzielenia przez godzine to minuty
+    seconds = (minutes - int(minutes)) * 60
+    print "%s:%s:%s" % (print0(hours), print0(minut), print0(seconds))
 
-			czas = tempo*dystans # wynik w minutach
-			minutes2Time(czas)
 
-			pauseEnd()
+def pause_end():
+    '''control app: wait for input'''
+    raw_input("Press any key to back to menu.")
 
-		elif odp == 4:
-			cls()
-			vdotCalc()
-			pauseEnd()
 
-		elif odp == 5:
-			print "End with Calculations\nSee u later :)."
-			exit(0)
-		else:
-			print "End with Calculations\nSee u later :)."
-			exit(0)
+def menu():
+    ''' Main function of program, at startup
+    it display menu, wait for input and control flow of application'''
+    while True:
+        cls()
+        print MENU_TEXT
+        odp = int(raw_input("What do you want to calculate?\n>> "))
+        cls()
 
-menu()
+        if odp == 1:
+            print LONGRUN_TEXT
+            dystans = float(
+                raw_input("How many kilometres do you run in this week?\n>> "))
+            dane_tempa = raw_input("What is your long run pace?\n>> ")
+
+            tempo = string_tempo(dane_tempa)
+            lsd = (dystans / 4) / (0.75)
+            czas = lsd * tempo
+
+            if czas > 120:
+                print "Run in comfortable way for 2 hours."
+            else:
+                print "Your long run in this week should be: %dkm" % lsd
+                print "Probably your run will takes: %d minutes" % czas
+            
+        elif odp == 2:
+            print "Distance & Time -> Pace"
+            print "=" * 50
+
+            dystans = float(raw_input("Distance[km]: "))
+            dane_czas = raw_input("Time[hh:mm:ss]: ")
+            czas = string_time(dane_czas)
+
+            pace = czas / dystans
+            minutes = int(pace)
+            sec = int((pace - minutes) * 60)
+
+            if sec > 9:
+                print "\nIf you ran %.2f km in %s, your pace will be %d:%d min/km" % (dystans, dane_czas, minutes, sec)
+            else:
+                print "\nIf you ran %.2f km in %s, your pace will be %d:0%d min/km" % (dystans, dane_czas, minutes, sec)
+
+        elif odp == 3:
+            print "Pace & Distance -> Time"
+            print "=" * 50
+
+            dane_tempa = raw_input("Pace[min/km]: ")
+            tempo = string_tempo(dane_tempa)
+            dystans = float(raw_input("Distance[km]: "))
+
+            czas = tempo * dystans  # wynik w minutach
+            minutes2time(czas)
+
+        elif odp == 4:
+            from vdot import vdot_calc
+            vdot_calc()
+
+        else:
+            print "End with Calculations\nSee u later :)."
+            exit(0)
+
+        pause_end()
+
+if __name__ == '__main__':
+    menu()
--- vdot.py
+++ vdot.py
@@ -1,40 +1,43 @@
+'''Implement VDOT calculation and print training&racing paces
+Required files: daniels_table_races.csv & paces.csv'''
 import pandas as pd
-vdot = pd.read_csv('daniels_table_races.csv',
+VDOT = pd.read_csv('daniels_table_races.csv',
                    delimiter=',', index_col='Params')
-pacesTab = pd.read_csv('paces.csv', delimiter=',', index_col='Params')
+PACES_TAB = pd.read_csv('paces.csv', delimiter=',', index_col='Params')
 
 
-def compareTimMin(time, timeList, distance):
+def compare_tim_min(time, time_list, distance):
+    '''Return nearest time from vdot table to time given by user'''
     test = len(time.split(':'))
 
     if test == 2 and distance <= 7:
-        list2 = [float('.'.join(i.split(':'))) for i in timeList]
+        list2 = [float('.'.join(i.split(':'))) for i in time_list]
         time2 = float('.'.join(time.split(':')))
         out = str(min(list2, key=lambda x: abs(x - time2)))
         out = ':'.join(out.split('.'))
         if len(out) == 4:
             out += '0'
-        ind = vdot.iloc[:, distance - 1][vdot.iloc[:,distance - 1] == out].index.tolist()[0]
+        ind = VDOT.iloc[:, distance - 1][VDOT.iloc[:, distance - 1] == out].index.tolist()[0]
         return out, ind
     elif test == 2 and distance > 7:
         print "Error: time must be in format(hh:mm:ss)"
         return "", ""
     elif test == 3 and distance > 7:
-        list2 = [int(''.join(i.split(':'))) for i in timeList]
+        list2 = [int(''.join(i.split(':'))) for i in time_list]
         time2 = int(''.join(time.split(':')))
         out = str(min(list2, key=lambda x: abs(x - time2)))
         out = out[:1] + ':' + out[1:3] + ':' + out[3:]
-        ind = vdot.iloc[:, distance - 1][vdot.iloc[:,distance - 1] == out].index.tolist()[0]
+        ind = VDOT.iloc[:, distance - 1][VDOT.iloc[:, distance - 1] == out].index.tolist()[0]
         return out, ind
     elif test == 3 and distance <= 7:
         print "Error: time must be in format(mm:ss)"
         return "", ""
 
 
-def vdotCalc():
+def vdot_calc():
     # Race time depends on VDOT
 
-    print '{:~^20}'.format('Vdot Table')
+    print '{:~^20}'.format('VDOT Table')
     print "Choose distance:"
     print """
     1. 1500m
@@ -48,7 +51,7 @@ def vdotCalc():
     9. Marathon
     """
     distance = int(raw_input("\n>> "))
-    distDic = {
+    dist_dic = {
         1: '1500m',
         2: '1600m',
         3: '3km',
@@ -62,12 +65,12 @@ def vdotCalc():
 
     time = raw_input("Provide your time: ")
 
-    timeTab, ind = compareTimMin(time, vdot[distDic[distance]], distance)
-    # print "NEAREST TIME: ", timeTab
+    time_tab, ind = compare_tim_min(time, VDOT[dist_dic[distance]], distance)
+    # print "NEAREST TIME: ", time_tab
     print "YOUR VDOT IS: ", ind,"\n"
     print '{:~^20}'.format('PACES')
-    print pacesTab.ix[ind].to_frame()
+    print PACES_TAB.ix[ind].to_frame()
     print ""
     print '{:~^20}'.format('RACING TIMES')
-    print vdot.ix[ind].to_frame().T
+    print VDOT.ix[ind].to_frame().T
     print ""
